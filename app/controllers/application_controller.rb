655b9e06512bc817d39aa3ab7fb029eaefb5fabf
Jono Sommers <jonosommers95@gmail.com>
2025-04-29 10:45:59 -0500

    updated status code for render_not_found method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   private
 
   def render_not_found(error)
-    render json: ErrorSerializer.format_error(ErrorMessage.new(error.message, 422)), status: :not_found
+    render json: ErrorSerializer.format_error(ErrorMessage.new(error.message, 404)), status: :not_found
   end
 end
